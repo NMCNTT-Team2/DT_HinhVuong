df21d324da39db35965fb43df72f78d9fb08c853
CakeB0y <74585538+CakeB0y@users.noreply.github.com>
2020-12-10 23:48:39 +0700

    Create DTHinhVuong.cpp

---
+++ DTHinhVuong.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N, S;
+	cout << "Nhap chieu dai canh: ";
+	cin >> N;
+	S = N * N;
+	cout << "Dien tich hinh vuong: " << S;
+	return 0;
+}
